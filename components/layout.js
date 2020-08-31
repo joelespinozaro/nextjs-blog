b8c17222d5788b2a4cee12d669e368482591b040
joelespinozaro <joelespinozaro@gmail.com>
2020-08-30 20:12:56 -0500

    Add _document.js

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = "Joel Espinoza Rojas"
-export const siteTitle = "Next.js Sample Website"
+export const siteTitle = "Joel Espinoza Rojas Blog"
 
 export default function Layout({ children, home }) {
     return (
